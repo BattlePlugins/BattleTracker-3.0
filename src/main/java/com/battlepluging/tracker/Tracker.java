65ad4fbee324a796aafd4f4cadf9d96433e24d9b
Zach Fiorito <zbfiorito@gmail.com>
2019-06-25 19:56:09 -0500

    Add pom and basic file structure

---
+++ src/main/java/com/battlepluging/tracker/Tracker.java
@@ -0,0 +1,18 @@
+package com.battlepluging.tracker;
+
+import mc.alk.v1r7.core.MCPlugin;
+
+public class Tracker extends MCPlugin {
+
+    @Override
+    public void onEnable() {
+
+        super.onEnable();
+
+    }
+
+    @Override
+    public void onDisable() {
+        
+    }
+}
