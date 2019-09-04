077e7a8469ad8fdfc71e4161afebe200d980312b
RednedEpic <redned235@gmail.com>
2019-09-03 22:40:45 -0500

    Save records on plugin disable

--- src/main/java/org/battleplugins/tracker/bukkit/BukkitTrackerPlugin.java
+++ src/main/java/org/battleplugins/tracker/bukkit/BukkitTrackerPlugin.java
@@ -2,6 +2,7 @@ package org.battleplugins.tracker.bukkit;
 
 import mc.alk.battlecore.bukkit.BukkitBattlePlugin;
 import org.battleplugins.tracker.BattleTracker;
+import org.battleplugins.tracker.TrackerInterface;
 import org.battleplugins.tracker.TrackerManager;
 import org.battleplugins.tracker.bukkit.listener.PvEListener;
 import org.battleplugins.tracker.bukkit.listener.PvPListener;
@@ -15,11 +16,13 @@ import org.bukkit.plugin.ServicePriority;
  */
 public class BukkitTrackerPlugin extends BukkitBattlePlugin {
 
+    private BattleTracker tracker;
+
     @Override
     public void onEnable() {
         super.onEnable();
 
-        BattleTracker tracker = new BattleTracker(this);
+        tracker = new BattleTracker(this);
         BattleTracker.setInstance(tracker);
         // Register the tracker manager into the service provider API
         getServer().getServicesManager().register(TrackerManager.class, tracker.getTrackerManager(), this, ServicePriority.Normal);
@@ -38,5 +41,17 @@ public class BukkitTrackerPlugin extends BukkitBattlePlugin {
     @Override
     public void onDisable() {
         super.onDisable();
+
+        getMCLogger().info("Saving all records...");
+        try {
+            for (TrackerInterface trackerInterface : tracker.getTrackerManager().getInterfaces().values()) {
+                trackerInterface.saveAll();
+            }
+            getMCLogger().info("Saved all records successfully!");
+        } catch (Exception ex) {
+            getMCLogger().error("Could not save all records! Please make sure everything is configured correctly!");
+            getMCLogger().error("If this error persists, please open a report on GitHub!");
+            ex.printStackTrace();
+        }
     }
 }
--- src/main/java/org/battleplugins/tracker/sponge/SpongeTrackerPlugin.java
+++ src/main/java/org/battleplugins/tracker/sponge/SpongeTrackerPlugin.java
@@ -3,6 +3,7 @@ package org.battleplugins.tracker.sponge;
 import mc.alk.battlecore.sponge.SpongeBattlePlugin;
 import org.battleplugins.tracker.BattleTracker;
 import org.battleplugins.tracker.TrackerInfo;
+import org.battleplugins.tracker.TrackerInterface;
 import org.battleplugins.tracker.TrackerManager;
 import org.battleplugins.tracker.sponge.listener.PvEListener;
 import org.battleplugins.tracker.sponge.listener.PvPListener;
@@ -18,11 +19,13 @@ import org.spongepowered.api.plugin.Plugin;
 @Plugin(id = "bt", authors = "BattlePlugins", name = TrackerInfo.NAME, version = TrackerInfo.VERSION, description = TrackerInfo.DESCRIPTION, url = TrackerInfo.URL)
 public class SpongeTrackerPlugin extends SpongeBattlePlugin {
 
+    private BattleTracker tracker;
+
     @Override
     public void onEnable() {
         super.onEnable();
 
-        BattleTracker tracker = new BattleTracker(this);
+        tracker = new BattleTracker(this);
         BattleTracker.setInstance(tracker);
 
         // Register the tracker manager into the service provider API
@@ -41,6 +44,16 @@ public class SpongeTrackerPlugin extends SpongeBattlePlugin {
 
     @Override
     public void onDisable() {
-
+        getMCLogger().info("Saving all records...");
+        try {
+            for (TrackerInterface trackerInterface : tracker.getTrackerManager().getInterfaces().values()) {
+                trackerInterface.saveAll();
+            }
+            getMCLogger().info("Saved all records successfully!");
+        } catch (Exception ex) {
+            getMCLogger().error("Could not save all records! Please make sure everything is configured correctly!");
+            getMCLogger().error("If this error persists, please open a report on GitHub!");
+            ex.printStackTrace();
+        }
     }
 }
