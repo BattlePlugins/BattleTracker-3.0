c25a41746b6aa19e8d984c40c446340401e29cde
RednedEpic <redned235@gmail.com>
2019-09-06 23:20:01 -0500

    Add the rest of the translatable/configurable messages (Closes #11)

--- src/main/java/org/battleplugins/tracker/executor/TrackerExecutor.java
+++ src/main/java/org/battleplugins/tracker/executor/TrackerExecutor.java
@@ -1,7 +1,7 @@
 package org.battleplugins.tracker.executor;
 
+import mc.alk.battlecore.controllers.MessageController;
 import mc.alk.battlecore.executor.CustomCommandExecutor;
-import mc.alk.mc.ChatColor;
 import mc.alk.mc.MCOfflinePlayer;
 import mc.alk.mc.command.MCCommandSender;
 import org.battleplugins.tracker.BattleTracker;
@@ -21,7 +21,6 @@ import java.util.UUID;
  *
  * @author Redned
  */
-// TODO: Make all messages in here configurable options
 public class TrackerExecutor extends CustomCommandExecutor {
 
     private BattleTracker tracker;
@@ -39,7 +38,8 @@ public class TrackerExecutor extends CustomCommandExecutor {
 
     @MCCommand(cmds = "top")
     public void topCommand(MCCommandSender sender, int amount) {
-        sender.sendMessage(ChatColor.RED + "==== " + ChatColor.YELLOW + interfaceName + " Leaderboards" + ChatColor.RED + " ====");
+        MessageManager messageManager = tracker.getMessageManager();
+        sender.sendMessage(MessageController.colorChat(messageManager.getMessage("leaderboardHeader").replace("%tracker%", interfaceName)));
         Map<UUID, Record> records = tracker.getTrackerManager().getInterface(interfaceName).getRecords();
         Map<Record, Float> unsortedRecords = new HashMap<>();
         records.forEach((uuid, record) -> unsortedRecords.put(record, record.getRating()));
@@ -49,10 +49,14 @@ public class TrackerExecutor extends CustomCommandExecutor {
 
         int i = 1;
         for (Map.Entry<Record, Float> recordEntry : sortedRecords.entrySet()) {
-            sender.sendMessage(ChatColor.GOLD + "#" + i + " " + ChatColor.YELLOW + recordEntry.getKey().getName() + " - "
-                    + ChatColor.AQUA + (int) recordEntry.getKey().getRating() + " "
-                    + ChatColor.RED + "Kills: " + ChatColor.GOLD + (int) recordEntry.getKey().getStat(StatType.KILLS)
-                    + ChatColor.RED + " Deaths: " + ChatColor.GOLD + (int) recordEntry.getKey().getStat(StatType.DEATHS));
+            String message = messageManager.getMessage("leaderboardText");
+            message = message.replace("%ranking%", String.valueOf(i));
+            message = message.replace("%rating%", String.valueOf((int) recordEntry.getKey().getRating()));
+            message = message.replace("%kills%", String.valueOf((int) recordEntry.getKey().getStat(StatType.KILLS)));
+            message = message.replace("%deaths%", String.valueOf((int) recordEntry.getKey().getStat(StatType.DEATHS)));
+            message = message.replace("%player_name%", recordEntry.getKey().getName());
+            message = message.replace("%tracker%", interfaceName);
+            sender.sendMessage(MessageController.colorChat(message));
 
             // limit at 100 to prevent lag and spam
             if (i >= amount || i >= 100)
@@ -72,10 +76,16 @@ public class TrackerExecutor extends CustomCommandExecutor {
             return;
         }
 
-        sender.sendMessage(ChatColor.YELLOW + player.getName() + " - " + ChatColor.AQUA + (int) record.getRating()
-                + ChatColor.YELLOW + " (Max Rating: " + (int) record.getStat(StatType.MAX_RATING) + ") " + ChatColor.RED + "Kills: "
-                + ChatColor.GOLD + (int) record.getStat(StatType.KILLS) + ChatColor.RED + " Deaths: " + ChatColor.GOLD + (int) record.getStat(StatType.DEATHS)
-                + ChatColor.RED + " KDR: " + ChatColor.YELLOW + (record.getStat(StatType.KILLS) / record.getStat(StatType.DEATHS)));
+        // Replace this seperately since everything else is parsed as an int, only temporary for now
+        // TODO: Address this later
+        String message = messageManager.getFormattedMessage(player, "rankingText");
+        message = message.replace("%kd_ratio%", String.valueOf(record.getStat(StatType.KD_RATIO)));
+
+        for (StatType type : StatType.values()) {
+            message = message.replace("%" + type.getInternalName() + "%", String.valueOf((int) record.getStat(type)));
+        }
+
+        sender.sendMessage(message);
     }
 
     @MCCommand(cmds = "reset", perm = "battletracker.reset")
@@ -88,8 +98,7 @@ public class TrackerExecutor extends CustomCommandExecutor {
         }
 
         trackerInterface.createNewRecord(player);
-
-        sender.sendMessage(ChatColor.GREEN + "Reset " + player.getName() + "'s " + trackerInterface.getName() + " data!");
+        sender.sendMessage(messageManager.getFormattedMessage(player, "recordsReset").replace("%tracker%", trackerInterface.getName()));
     }
 
     @MCCommand(cmds = "set", perm = "battletracker.set")
@@ -103,11 +112,11 @@ public class TrackerExecutor extends CustomCommandExecutor {
         }
 
         if (!record.getStats().containsKey(statType.toLowerCase())) {
-            sender.sendMessage(ChatColor.RED + "This stat type does not exist in this tracker!");
+            sender.sendMessage(messageManager.getFormattedMessage("statNotInTracker"));
             return;
         }
 
         trackerInterface.setValue(statType, value, player);
-        sender.sendMessage(ChatColor.GREEN + "Set " + player.getName() + "'s " + statType.toLowerCase() + " value to " + value + "!");
+        sender.sendMessage(messageManager.getFormattedMessage(player, "setStatValue").replace("%stat%", statType.toLowerCase()).replace("%value%", String.valueOf(value)));
     }
 }
